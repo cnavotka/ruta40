0bc589d01d7120ad665c5174b452313229e4f867
Carlos Navotka <knavotka@hotmail.com>
2021-06-05 13:43:14 +0000

    added mark to center google maps in the middle of ruta 40 bariloche

--- maps.js
+++ maps.js
@@ -1,2 +1,15 @@
 // Initialize and add the map
-function initMap() 
+function initMap() {
+    // The location of Bariloche
+    const bariloche = { lat: -41.12015054243244, lng: -71.30249462122084 };
+    // The map, centered at Bariloche
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: bariloche,
+    });
+    // The marker, positioned at Bariloche
+    const marker = new google.maps.Marker({
+      position: bariloche,
+      map: map,
+    });
+  }
