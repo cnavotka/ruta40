1db398716c0e299591deaa46c496d814f6782d07
Carlos Navotka <knavotka@hotmail.com>
2021-06-05 16:37:48 +0000

    new markers added to google maps in maps js file

--- maps.js
+++ maps.js
@@ -1,15 +1,59 @@
-// Initialize and add the map
-function initMap() {
-    // The location of Bariloche
-    const bariloche = { lat: -41.12015054243244, lng: -71.30249462122084 };
-    // The map, centered at Bariloche
+
+  
+  // Initalize map centered in Bariloche
+  function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: bariloche,
+      zoom: 3,
+      center: { lat: -41.12015054243244, lng: -71.30249462122084 },
     });
-    // The marker, positioned at Bariloche
-    const marker = new google.maps.Marker({
-      position: bariloche,
-      map: map,
+    // Create an array of alphabetical characters used to label the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    const markers = locations.map((location, i) => {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+      });
+    });
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
   }
+  // first location world ends lighthouse
+  const locations = [
+    { lat: -54.73363, lng: -63.855522},
+    // ushuaia
+    { lat: -54.71473, lng: -68.85522},
+    // perito moreno
+    { lat: -50.47430, lng: -73.04661 },
+    // la leona
+    { lat: -49.80550, lng: -72.05352 },
+    // el chalten
+    { lat: -49.33099, lng: -72.89517},
+    // bariloche
+    { lat: -41.12015054243244, lng: -71.30249462122084 },
+    // mendoza
+    { lat: -32.86350, lng: -68.86844 },
+    // valle de la luna
+    { lat: -30.17790, lng: -67.86721},
+    // leoncito
+    { lat: -31.79041, lng: -69.34649},
+    // talampaya
+    { lat:-29.89607, lng: -67.85554},
+    // cafayate
+    {lat: -26.07449, lng: -65.97087},
+    // los cardones
+    {lat: -25.19109, lng: -66.02992},
+    // purmamarca
+    {lat: -23.74877, lng: -65.50682},
+    // salinas grandes
+    {lat: -23.58841, lng: -65.93879},
+    // cuesta
+    {lat: -25.18712, lng: -65.84746},
+];
+  
