56849ec81b5203ddb22fca2737654f967a09ce29
Carlos Navotka <knavotka@hotmail.com>
2021-07-20 15:12:48 +0000

    Fix spelling mistake in line 3 wrong documen typo there

--- modal.js
+++ modal.js
@@ -1,6 +1,6 @@
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
-const close = documen.getElementById('close');
+const close = document.getElementById('close');
 
 open.addEventListener('click', () => {
     modal_container.classList.add('show');
