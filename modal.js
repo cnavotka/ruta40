2e71bd1826fa32541bedbbafd037ecce82b329bb
Carlos Navotka <knavotka@hotmail.com>
2021-07-24 13:44:14 +0000

    Add line to the modal js file to clear the contact form after submit

--- modal.js
+++ modal.js
@@ -1,9 +1,11 @@
+// Adapted from Code Institute tutorial Walkthrough Project
 function sendMail(contactForm) {
     emailjs.send("service_agqog5v", "template_viast2c", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "comment": contactForm.comment.value
     })
+
     .then(
         function(response) {
             console.log("SUCCESS", response);
@@ -12,5 +14,8 @@ function sendMail(contactForm) {
             console.log("FAILED", error);
         }
     );
+
+    // To clear the form
+    document.getElementById('contactForm').request();
     return false;  // To block from loading a new page
 }
